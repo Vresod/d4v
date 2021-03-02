75a853e708ec4cc1f898f27e119fb5ad82753864
gamererr <starkid2u@outlook.com>
2021-03-02 13:30:19 -0500

    dpy extentions now ig

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from discord.ext import commands
 import discord
 import random
 import health
 
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='d4v!', help_command=None, intents=intents)
 
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
@@ -13,36 +14,29 @@ with open("tokenfile", "r") as tokenfile:
 async def on_ready():
 	print('Dojyan!')
 
-prefix = "d4v!"
+@client.command()
+async def reserve(ctx):
 
-@client.event
-async def on_message(message):
-
-	theworld = discord.utils.get(message.guild.roles, id=816244945580982282)
-	thehand = discord.utils.get(message.guild.roles, id=816244947761102850)
-	starplatinum = discord.utils.get(message.guild.roles, id=816244946365186048)
-	silverchariot = discord.utils.get(message.guild.roles, id=816244947224100864)
+	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
+	thehand = discord.utils.get(ctx.guild.roles, id=816244947761102850)
+	starplatinum = discord.utils.get(ctx.guild.roles, id=816244946365186048)
+	silverchariot = discord.utils.get(ctx.guild.roles, id=816244947224100864)
 
 	stands = [thehand, theworld, starplatinum, silverchariot]
 
-	argsraw = message.content.lower().replace(prefix, "")
-	args = argsraw.split(" ")
-
-	if message.content.lower().startswith(prefix):
-		if args[0] == "reserve" or args[0] == "r":
-			try:
-				standwanted = message.role_mentions[0]
-			except IndexError:
+	try:
+		standwanted = ctx.message.role_mentions[0]
+	except IndexError:
+		return
+	if standwanted in stands:
+		for x in stands:
+			if x in ctx.author.roles:
+				await ctx.channel.send(f"you already have {x.name}")
 				return
-			if standwanted in stands:
-				for x in stands:
-					if x in message.author.roles:
-						await message.channel.send(f"you already have {x.name}")
-						return
-				if standwanted.members == []:
-					await message.author.add_roles(standwanted)
-					await message.channel.send(f"you now have {standwanted.name}!")
-				else:
-					await message.channel.send(f"{standwanted.members[0].name} already has {standwanted.name}")
+		if standwanted.members == []:
+			await ctx.author.add_roles(standwanted)
+			await ctx.channel.send(f"you now have {standwanted.name}!")
+		else:
+			await ctx.channel.send(f"{standwanted.members[0].name} already has {standwanted.name}")
 
 client.run(token)
