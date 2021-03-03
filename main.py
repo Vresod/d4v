bef22600aa718968cf11eea667c3b869da536300
gamererr <starkid2u@outlook.com>
2021-03-03 10:42:00 -0500

    error handling

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def on_ready():
 	print('Dojyan!')
 
 @client.command(aliases=["r"],brief="reserve a stand")
-async def reserve(ctx):
+async def reserve(ctx, mentionrole):
 
 	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
 	thehand = discord.utils.get(ctx.guild.roles, id=816244947761102850)
@@ -66,7 +66,7 @@ async def reserve(ctx):
 
 # @commands.cooldown(1, getcooldown(), commands.BucketType.user)
 @client.command(aliases=["p"],brief="ORAAAA!")
-async def punch(ctx):
+async def punch(ctx, mentionuser):
 
 	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
 	thehand = discord.utils.get(ctx.guild.roles, id=816244947761102850)
@@ -94,7 +94,7 @@ async def punch(ctx):
 
 
 @client.command(aliases=["i"],brief="get info on a user's health and stand")
-async def info(ctx):
+async def info(ctx, mentionuser):
 
 	with open("health.json", "rt") as healthraw: # opens health.json
 		health = json.loads(healthraw.read()) # sets the health varible to the contents of health.json
@@ -132,7 +132,20 @@ async def info(ctx):
 
 
 
-
+@client.event
+async def on_command_error(ctx:commands.Context, exception):
+    embed = discord.Embed(color=discord.Color.red())
+    if type(exception) is commands.errors.MissingRequiredArgument:
+        embed.title = "You forgot an argument"
+        embed.description = f"The syntax to `{client.command_prefix}{ctx.command.name}` is `{client.command_prefix}{ctx.command.name} {ctx.command.signature}`."
+        await ctx.send(embed=embed)
+    elif type(exception) is commands.CommandNotFound:
+        embed.title = "Invalid command"
+        embed.description = f"The command you just tried to use is invalid. Use `{client.command_prefix}help` to see all commands."
+        await ctx.send(embed=embed)
+    else:
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
 
 @client.event
 async def on_member_join(member):
