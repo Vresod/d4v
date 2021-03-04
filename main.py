30fa088d1968b94d1c1a1885ba0c886eaf48be00
Vampante <72152382+Vampante@users.noreply.github.com>
2021-03-04 20:23:01 +0000

    added time stop, star finger and cool embed things
    star finger does 10 damage and hits the guy that you mention:
    example: d4v!starfinger @Gamererr
    
    d4v!timestop to stop time, the world's timestop is 9 seconds and star platinum's time stop is 4, the bot will start sending embed messages when there are 3 seconds remaining

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import health
 import json
 import asyncio
 
+
+
+
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='d4v!', intents=intents)
 
@@ -12,6 +15,19 @@ client = commands.Bot(command_prefix='d4v!', intents=intents)
 async def on_member_join(member):
 	cantstoptime = discord.utils.get(guild.roles, id=816382645964636180),
 	await member.add_roles(cantstoptime)
+
+
+
+@client.command(aliases=["sf","starfinger"],brief="finger goes brrr")
+async def star_finger(ctx,member):
+	target = ctx.mentions[0]
+	user = ctx.author
+	starplatinum = discord.utils.get(ctx.guild.roles, id=816244946365186048)
+	if not starplatinum in ctx.author.roles:
+		return
+	await health.changehealth(user=target,add=0,subtract=10)
+	await ctx.send(f'{user} used Star Finger on {target}!')
+
 
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
@@ -27,28 +43,29 @@ async def reserve(ctx):
 	starplatinum = discord.utils.get(ctx.guild.roles, id=816244946365186048)
 	silverchariot = discord.utils.get(ctx.guild.roles, id=816244947224100864)
 	crazydiamond = discord.utils.get(ctx.guild.roles, id=816602875014676480)
-
 	cantstoptime = discord.utils.get(ctx.guild.roles, id=816382645964636180)
-
 	stands = [thehand, theworld, starplatinum, silverchariot, crazydiamond]
-	
-	for x in stands:
-		if x in ctx.author.roles:
-			await ctx.channel.send(f"you have lost {x.name}")
-			await ctx.author.add_roles(cantstoptime)
-			await ctx.author.remove_roles(x)
-			return
 	try:
 		standwanted = ctx.message.role_mentions[0]
 	except IndexError:
 		return
-
 	if standwanted in stands:
+		for x in stands:
+			if x in ctx.author.roles:
+				await ctx.author.remove_roles(thehand)
+				await ctx.author.remove_roles(silverchariot)
+				await ctx.author.remove_roles(crazydiamond)
+				await ctx.author.remove_roles(theworld)
+				await ctx.author.remove_roles(starplatinum)
+				await ctx.author.add_roles(cantstoptime)
+				return
 		if standwanted.members == []:
-			if standwanted == theworld or standwanted == starplatinum:
-				await ctx.author.remove_roles(cantstoptime)
 			await ctx.author.add_roles(standwanted)
-			await ctx.channel.send(f"you now have {standwanted.name}!")
+			await ctx.channel.send(f"You now have {standwanted.name}!")
+			if standwanted == theworld:
+				await ctx.author.remove_roles(cantstoptime)
+			if standwanted == starplatinum:
+				await ctx.author.remove_roles(cantstoptime)
 		else:
 			await ctx.channel.send(f"{standwanted.members[0].name} already has {standwanted.name}")
 
@@ -56,22 +73,69 @@ async def reserve(ctx):
 async def time_stop(ctx):
 	channel = ctx.channel
 	guild = ctx.guild
-	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
 	starplatinum = discord.utils.get(ctx.guild.roles, id=816244946365186048)
+	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
 	cantstoptime = discord.utils.get(ctx.guild.roles, id=816382645964636180)
+	if not theworld in ctx.author.roles:
+		if not starplatinum in ctx.author.roles:
+			return
 	if theworld in ctx.author.roles:
-		await ctx.channel.category.set_permissions(guild.get_role(816382645964636180), send_messages=False, read_messages=False)
-		await channel.send("time stopped")
-		await asyncio.sleep(9)
-		await ctx.channel.category.set_permissions(guild.get_role(816382645964636180), send_messages=True, read_messages=True)
-		await channel.send("time resumed")
-	elif starplatinum in ctx.author.roles:
-		await ctx.channel.category.set_permissions(guild.get_role(816382645964636180), send_messages=False, read_messages=False)
-		await channel.send("time stopped")
-		await asyncio.sleep(5)
-		await ctx.channel.category.set_permissions(guild.get_role(816382645964636180), send_messages=True, read_messages=True)
-		await channel.send("time resumed")
+		await ctx.channel.set_permissions(guild.get_role(816382645964636180), send_messages=False, read_messages=False)
+		await asyncio.sleep(6)
+		embed = discord.Embed(title="3 seconds...", colour=discord.Colour(0xdbf353))
+		await ctx.channel.send(embed=embed)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="2 seconds...", colour=discord.Colour(0xf3ce53))
+		await ctx.channel.send(embed=embed)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="1 second...", colour=discord.Colour(0xf3564e))
+		await ctx.channel.send(embed=embed)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="Time resumes!", colour=discord.Colour(0x77140e))
+		await ctx.channel.send(embed=embed)
+		await ctx.channel.set_permissions(guild.get_role(816382645964636180), send_messages=True, read_messages=True)
+	if starplatinum in ctx.author.roles:
+		await ctx.channel.set_permissions(guild.get_role(816382645964636180), send_messages=False, read_messages=False)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="3 seconds...", colour=discord.Colour(0xdbf353))
+		await ctx.channel.send(embed=embed)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="2 seconds...", colour=discord.Colour(0xf3ce53))
+		await ctx.channel.send(embed=embed)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="1 second...", colour=discord.Colour(0xf3564e))
+		await ctx.channel.send(embed=embed)
+		await asyncio.sleep(1)
+		embed = discord.Embed(title="Time resumes!", colour=discord.Colour(0x77140e))
+		await ctx.channel.send(embed=embed)
+		await ctx.channel.set_permissions(guild.get_role(816382645964636180), send_messages=True, read_messages=True)
 
+
+
+
+
+#def getcooldown(): 
+#	
+#	joe = discord.utils.get(client.guilds, id=814836159821250661)
+#	
+#	for x in joe.channels:
+#		ctx = x.history[0]
+#
+#		if not ctx.content.startswith("d4v!"):
+#			continue
+#
+#		if theworld in ctx.author.roles:
+#			return 3
+#		elif thehand in ctx.author.roles:
+#			return 4
+#		elif starplatinum in ctx.author.roles:
+#			return 3
+#		elif silverchariot in ctx.author.roles:
+#			return 1
+#		elif crazydiamond in ctx.author.roles:
+#			return 2
+
+# @commands.cooldown(1, getcooldown(), commands.BucketType.user)
 @client.command(aliases=["p"],brief="ORAAAA!")
 async def punch(ctx):
 
@@ -111,7 +175,7 @@ async def info(ctx):
 	silverchariot = discord.utils.get(ctx.guild.roles, id=816244947224100864)
 	crazydiamond = discord.utils.get(ctx.guild.roles, id=816602875014676480)
 
-	stands = [thehand, theworld, starplatinum, silverchariot, crazydiamond]
+	stands = [thehand, theworld, starplatinum, silverchariot, silverchariot]
 
 	if ctx.message.mentions == []:
 		InfoOn = ctx.author
@@ -121,32 +185,9 @@ async def info(ctx):
 	for x in stands:
 		if x in InfoOn.roles:
 			try:
-				embed = discord.Embed(title=f"{InfoOn}'s Info'", colour=discord.Colour(0xf8e257), description=f"health: {health[str(InfoOn.id)]} hp\nstand: {x.name}")
-				await ctx.channel.send(embed=embed)
+				await ctx.message.channel.send(f"{InfoOn} has `{health[str(InfoOn.id)]}` health\n{InfoOn}'s stand is {x.name}")
 			except KeyError:
 				await ctx.message.channel.send(f"{InfoOn}'s stand is {x.name}")
-				await ctx.channel.send(embed=embed)
 			return
-		elif x == stands[-1]:
-			embed = discord.Embed(title=f"{InfoOn}'s Info'", colour=discord.Colour(0xf8e257), description=f"health: 200 hp\nstand: none")
-			await ctx.channel.send(embed=embed)
-
-
-
-
-@client.event
-async def on_command_error(ctx:commands.Context, exception):
-    embed = discord.Embed(color=discord.Color.red())
-    if type(exception) is commands.errors.MissingRequiredArgument:
-        embed.title = "You forgot an argument"
-        embed.description = f"The syntax to `{client.command_prefix}{ctx.command.name}` is `{client.command_prefix}{ctx.command.name} {ctx.command.signature}`."
-        await ctx.send(embed=embed)
-    elif type(exception) is commands.CommandNotFound:
-        embed.title = "Invalid command"
-        embed.description = f"The command you just tried to use is invalid. Use `{client.command_prefix}help` to see all commands."
-        await ctx.send(embed=embed)
-    else:
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
 
 client.run(token)
