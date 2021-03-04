e2841fec58b9e2f699b8e21c8d34aa3fa5a5752d
Vampante <72152382+Vampante@users.noreply.github.com>
2021-03-04 15:31:24 +0000

    added crazy diamond (kinda) and like time stop but like also not

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from discord.ext import commands
 import discord
 import random
-from health import *
+import health
 import json
+import asyncio
+
+
+
+
+
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='d4v!', intents=intents)
+
+@client.event
+async def on_member_join(member):
+	cantstoptime = discord.utils.get(guild.roles, id=816382645964636180),
+	await member.add_roles(cantstoptime)
+
+
 
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
@@ -15,17 +28,13 @@ async def on_ready():
 	print('Dojyan!')
 
 @client.command(aliases=["r"],brief="reserve a stand")
-async def reserve(ctx, mentionrole):
-
+async def reserve(ctx):
 	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
 	thehand = discord.utils.get(ctx.guild.roles, id=816244947761102850)
 	starplatinum = discord.utils.get(ctx.guild.roles, id=816244946365186048)
 	silverchariot = discord.utils.get(ctx.guild.roles, id=816244947224100864)
 	crazydiamond = discord.utils.get(ctx.guild.roles, id=816602875014676480)
-	NoTimeStop = discord.utils.get(ctx.guild.roles, id=816382645964636180)
-
 	stands = [thehand, theworld, starplatinum, silverchariot, crazydiamond]
-
 	try:
 		standwanted = ctx.message.role_mentions[0]
 	except IndexError:
@@ -37,11 +46,26 @@ async def reserve(ctx, mentionrole):
 				return
 		if standwanted.members == []:
 			await ctx.author.add_roles(standwanted)
-			if standwanted == theworld or standwanted == starplatinum:
-				await ctx.author.remove_roles(NoTimeStop)
 			await ctx.channel.send(f"you now have {standwanted.name}!")
 		else:
 			await ctx.channel.send(f"{standwanted.members[0].name} already has {standwanted.name}")
+
+@client.command(aliases=["ts","st","timestop","stoptime","stop_time"],brief="za warudo toki wo tomare")
+async def time_stop(ctx):
+	channel = ctx.channel,
+	guild = ctx.guild,
+	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282),
+	cantstoptime = discord.utils.get(ctx.guild.roles, id=816382645964636180),
+	if not theworld in ctx.author.roles:
+		return
+	await ctx.channel.set_permissions(cantstoptime, send_messages=False)
+	asyncio.sleep(9)
+	await ctx.channel.set_permissions(cantstoptime, send_messages=True)
+
+
+
+
+
 
 #def getcooldown(): 
 #	
@@ -66,7 +90,7 @@ async def reserve(ctx, mentionrole):
 
 # @commands.cooldown(1, getcooldown(), commands.BucketType.user)
 @client.command(aliases=["p"],brief="ORAAAA!")
-async def punch(ctx, mentionuser):
+async def punch(ctx):
 
 	theworld = discord.utils.get(ctx.guild.roles, id=816244945580982282)
 	thehand = discord.utils.get(ctx.guild.roles, id=816244947761102850)
@@ -80,18 +104,17 @@ async def punch(ctx, mentionuser):
 	puncher = ctx.author
 
 	if theworld in puncher.roles:
-		await changehealth(user=punched, add=0, subtract=17)
+		await health.changehealth(user=punched, add=0, subtract=17)
 	elif thehand in puncher.roles:
-		await changehealth(user=punched, add=0, subtract=15)
+		await health.changehealth(user=punched, add=0, subtract=15)
 	elif starplatinum in puncher.roles:
-		await changehealth(user=punched, add=0, subtract=20)
+		await health.changehealth(user=punched, add=0, subtract=20)
 	elif silverchariot in puncher.roles:
-		await changehealth(user=punched, add=0, subtract=10)
+		await health.changehealth(user=punched, add=0, subtract=10)
 	elif crazydiamond in puncher.roles:
-		await changehealth(user=punched, add=0, subtract=14)
+		await health.changehealth(user=punched, add=0, subtract=14)
 
 	await ctx.channel.send(f"{puncher.name} punched {punched.name}")    
-
 
 @client.command(aliases=["i"],brief="get info on a user's health and stand")
 async def info(ctx):
@@ -105,7 +128,7 @@ async def info(ctx):
 	silverchariot = discord.utils.get(ctx.guild.roles, id=816244947224100864)
 	crazydiamond = discord.utils.get(ctx.guild.roles, id=816602875014676480)
 
-	stands = [thehand, theworld, starplatinum, silverchariot, crazydiamond]
+	stands = [thehand, theworld, starplatinum, silverchariot, silverchariot]
 
 	if ctx.message.mentions == []:
 		InfoOn = ctx.author
@@ -114,44 +137,10 @@ async def info(ctx):
 
 	for x in stands:
 		if x in InfoOn.roles:
-			InfoOnStand = x
-			break
-		else:
-			InfoOnStand = None
-	
-	try:
-		embed = discord.Embed(title=f"{InfoOn}'s Info", colour=discord.Colour(0xabe05f), description=f"Stand: {InfoOnStand}\nHealth: {health[str(InfoOn.id)]}")
-	except KeyError:
-		await changehealth(user=InfoOn, add=0, subtract=0)
-		with open("health.json", "rt") as healthraw: # reopens health.json
-			health = json.loads(healthraw.read()) # sets the health varible to the contents of health.json
-		embed = discord.Embed(title=f"{InfoOn}'s Info", colour=discord.Colour(0xabe05f), description=f"Stand: {InfoOnStand}\nHealth: {health[str(InfoOn.id)]}")
-
-		
-	await ctx.message.channel.send(embed=embed)
-
-
-
-@client.event
-async def on_command_error(ctx:commands.Context, exception):
-    embed = discord.Embed(color=discord.Color.red())
-    if type(exception) is commands.errors.MissingRequiredArgument:
-        embed.title = "You forgot an argument"
-        embed.description = f"The syntax to `{client.command_prefix}{ctx.command.name}` is `{client.command_prefix}{ctx.command.name} {ctx.command.signature}`."
-        await ctx.send(embed=embed)
-    elif type(exception) is commands.CommandNotFound:
-        embed.title = "Invalid command"
-        embed.description = f"The command you just tried to use is invalid. Use `{client.command_prefix}help` to see all commands."
-        await ctx.send(embed=embed)
-    else:
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
-
-@client.event
-async def on_member_join(member):
-	
-	NoTimeStop = discord.utils.get(member.guild.roles, id=816382645964636180)
-
-	await member.add_roles(NoTimeStop)
+			try:
+				await ctx.message.channel.send(f"{InfoOn} has `{health[str(InfoOn.id)]}` health\n{InfoOn}'s stand is {x.name}")
+			except KeyError:
+				await ctx.message.channel.send(f"{InfoOn}'s stand is {x.name}")
+			return
 
 client.run(token)
