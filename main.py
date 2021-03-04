f9c25693d86b30aaad62671de4eed0dbec60bea0
gamererr <starkid2u@outlook.com>
2021-03-04 14:04:43 -0500

    error handling

--- main.py
+++ main.py
@@ -126,4 +126,21 @@ async def info(ctx):
 				await ctx.message.channel.send(f"{InfoOn}'s stand is {x.name}")
 			return
 
+
+
+@client.event
+async def on_command_error(ctx:commands.Context, exception):
+    embed = discord.Embed(color=discord.Color.red())
+    if type(exception) is commands.errors.MissingRequiredArgument:
+        embed.title = "You forgot an argument"
+        embed.description = f"The syntax to `{client.command_prefix}{ctx.command.name}` is `{client.command_prefix}{ctx.command.name} {ctx.command.signature}`."
+        await ctx.send(embed=embed)
+    elif type(exception) is commands.CommandNotFound:
+        embed.title = "Invalid command"
+        embed.description = f"The command you just tried to use is invalid. Use `{client.command_prefix}help` to see all commands."
+        await ctx.send(embed=embed)
+    else:
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
+
 client.run(token)
