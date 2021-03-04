5a70e41cc980a742537590b08ae7994d35ee1cc8
Vampante <72152382+Vampante@users.noreply.github.com>
2021-03-04 21:48:32 +0000

    read description
    now d4v!p attacks the last person that sent a message that is not the one who punched or d4v they also have a cool embed when you attack that changes based on the stand!

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import json
 import asyncio
 
 
-
-
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='d4v!', intents=intents)
 
+
+
+@client.command()
+async def ping(ctx):
+	await ctx.send(f'Pong! {round(client.latency*1000)} ms')
+
+
 @client.event
-async def on_member_join(member):
+async def on_member_join(member,guild):
 	cantstoptime = discord.utils.get(guild.roles, id=816382645964636180),
 	await member.add_roles(cantstoptime)
 
@@ -45,18 +50,20 @@ async def reserve(ctx):
 	crazydiamond = discord.utils.get(ctx.guild.roles, id=816602875014676480)
 	cantstoptime = discord.utils.get(ctx.guild.roles, id=816382645964636180)
 	stands = [thehand, theworld, starplatinum, silverchariot, crazydiamond]
-	
-	for x in stands:
-		if x in ctx.author.roles:
-			await ctx.author.remove_roles(x)
-			await ctx.author.add_roles(cantstoptime)
-			await ctx.channel.send(f"you have lost {x.name}")
-			return
 	try:
 		standwanted = ctx.message.role_mentions[0]
 	except IndexError:
 		return
 	if standwanted in stands:
+		for x in stands:
+			if x in ctx.author.roles:
+				await ctx.author.remove_roles(thehand)
+				await ctx.author.remove_roles(silverchariot)
+				await ctx.author.remove_roles(crazydiamond)
+				await ctx.author.remove_roles(theworld)
+				await ctx.author.remove_roles(starplatinum)
+				await ctx.author.add_roles(cantstoptime)
+				return
 		if standwanted.members == []:
 			await ctx.author.add_roles(standwanted)
 			await ctx.channel.send(f"You now have {standwanted.name}!")
@@ -145,21 +152,67 @@ async def punch(ctx):
 
 	stands = [thehand, theworld, starplatinum, silverchariot, crazydiamond]
 
-	punched = ctx.message.mentions[0]
 	puncher = ctx.author
 
 	if theworld in puncher.roles:
-		await health.changehealth(user=punched, add=0, subtract=17)
-	elif thehand in puncher.roles:
-		await health.changehealth(user=punched, add=0, subtract=15)
-	elif starplatinum in puncher.roles:
-		await health.changehealth(user=punched, add=0, subtract=20)
-	elif silverchariot in puncher.roles:
-		await health.changehealth(user=punched, add=0, subtract=10)
-	elif crazydiamond in puncher.roles:
-		await health.changehealth(user=punched, add=0, subtract=14)
+		async for message in ctx.channel.history():
+			if(message == ctx.message) or (message.author == ctx.message.author) or (message.author == client.user):
+				continue
+			else:
+				previous_message = message
+				punched = previous_message.author
+				await health.changehealth(user=punched, add=0, subtract=17)
+				attack_embed = discord.Embed(title=f"{puncher.name} punched {punched.name}! MUDA!", colour=discord.Colour(0xffdc00))
+				await ctx.channel.send(embed=attack_embed)
+				break
 
-	await ctx.channel.send(f"{puncher.name} punched {punched.name}")    
+	if thehand in puncher.roles:
+		async for message in ctx.channel.history():
+			if(message == ctx.message) or (message.author == ctx.message.author) or (message.author == client.user):
+				continue
+			else:
+				previous_message = message
+				punched = previous_message.author
+				await health.changehealth(user=punched, add=0, subtract=15)
+				attack_embed = discord.Embed(title=f"{puncher.name} punched {punched.name}! BBVV!", colour=discord.Colour(0x5957db))
+				await ctx.channel.send(embed=attack_embed)
+				break
+	if starplatinum in puncher.roles:
+		async for message in ctx.channel.history():
+			if(message == ctx.message) or (message.author == ctx.message.author) or (message.author == client.user):
+				continue
+			else:
+				previous_message = message
+				punched = previous_message.author
+				await health.changehealth(user=punched, add=0, subtract=20)
+				attack_embed = discord.Embed(title=f"{puncher.name} punched {punched.name}! ORA!", colour=discord.Colour(0x9b20c2))
+				await ctx.channel.send(embed=attack_embed)
+				break
+	if silverchariot in puncher.roles:
+		async for message in ctx.channel.history():
+			if(message == ctx.message) or (message.author == ctx.message.author) or (message.author == client.user):
+				continue
+			else:
+				previous_message = message
+				punched = previous_message.author
+				await health.changehealth(user=punched, add=0, subtract=12)
+				attack_embed = discord.Embed(title=f"{puncher.name} attacked {punched.name}!", colour=discord.Colour(0xf5f5f5))
+				await ctx.channel.send(embed=attack_embed)
+				break
+	if crazydiamond in puncher.roles:
+		async for message in ctx.channel.history():
+			if(message == ctx.message) or (message.author == ctx.message.author) or (message.author == client.user):
+				continue
+			else:
+				previous_message = message
+				punched = previous_message.author
+				await health.changehealth(user=punched, add=0, subtract=15)
+				attack_embed = discord.Embed(title=f"{puncher.name} punched {punched.name}! DORA!", colour=discord.Colour(0xe277d5))
+				await ctx.channel.send(embed=attack_embed)
+				break
+
+
+
 
 @client.command(aliases=["i"],brief="get info on a user's health and stand")
 async def info(ctx):
